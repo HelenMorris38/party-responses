229009292d53193523d2f7d8f44445e4fdeda5b6
Helen Morris <morris_helen@hotmail.com>
2023-04-24 11:10:02 +0100

    remove blank line and move print responses

--- src/app.py
+++ src/app.py
@@ -19,12 +19,11 @@ def party_responses():
             person['id'] = str(count).zfill(3)
             person['name'] = name
             person['response'] = response
-            
             print('Thank you! Your response has been recorded.')
-            print(responses)
             with open(filename, 'w') as f:
                 responses.append(person)
                 json.dump(responses, f)
+            print(responses)
 party_responses()
 
 def list_of_dicts_to_json(list_of_dicts):
