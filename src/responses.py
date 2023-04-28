97e4ce2f179872b090f4e1cb115fe7e7c422453a
Helen Morris <morris_helen@hotmail.com>
2023-04-28 14:28:55 +0100

    add waiting response var

--- src/responses.py
+++ src/responses.py
@@ -4,12 +4,15 @@ with open('party-responses.json', 'r', encoding='utf-8') as responses:
     read_responses = json.load(responses)
     yes_count = 0
     no_count = 0
+    waiting_response = 0
     for line in read_responses:
         print(line)
         if line['response'] == 'yes':
             yes_count += 1
-        else:
+        elif line['response'] == 'no':
             no_count += 1
+        else:
+            waiting_response += 1
     
     print(f'{yes_count} children are able to come to the party')
     print(f'{no_count} children are not able to come to the party')
