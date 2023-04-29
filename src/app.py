5b2ba26f13b004576703478660de49d59f41cc15
Helen Morris <morris_helen@hotmail.com>
2023-04-29 08:15:22 +0100

    refactor to keep input out of the function

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 import json
 from os import path
 
-def party_responses():
+def party_responses(name, response):
     responses = []
     filename = 'party-responses.json'
     if not path.isfile(filename):
@@ -10,27 +10,26 @@ def party_responses():
     
     with open(filename) as f:
         responses = json.load(f)
-    
-    count = 0
-    while True:
-        count += 1
-        name = input('What is your name?\nEnter q at anytime to quit.\n')
-        if name == 'q':
-            break
-        able_to_attend = input('Can you come to the party?\nPlease enter yes or no.\n')
-        if able_to_attend == 'q':
-            break
-        else:
-            person = {}
-            person['name'] = name
-            person['response'] = able_to_attend
-            print('Thank you! Your response has been recorded.')
-            responses.append(person)
-            print(responses)
+        person = {}
+        person['name'] = name
+        person['response'] = response
+        responses.append(person)
 
     with open(filename, 'w') as f:
         json.dump(responses, f, indent=4)
 
-party_responses()
+
+count = 0
+while True:
+    count += 1
+    name = input('What is your name?\nEnter q at anytime to quit.\n')
+    if name == 'q':
+        break
+    able_to_attend = input('Can you come to the party?\nPlease enter yes or no.\n')
+    if able_to_attend == 'q':
+        break
+    else:
+        party_responses(name, able_to_attend)
+        print('Thank you! Your response has been recorded.')
 
 
