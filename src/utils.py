a60ab26ed4dc310e7db6f081dab0832093d951fa
Helen Morris <morris_helen@hotmail.com>
2023-04-29 20:18:44 +0100

    initial commit

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import json
+from os import path
+
+def party_responses(name, response):
+    responses = []
+    filename = 'party-responses.json'
+    if not path.isfile(filename):
+        with open(filename, 'w') as f:
+            json.dump(responses, f, indent=4)
+    
+    with open(filename) as f:
+        responses = json.load(f)
+        person = {}
+        person['name'] = name
+        person['response'] = response
+        responses.append(person)
+
+    with open(filename, 'w') as f:
+        json.dump(responses, f, indent=4)
