b10d63068ac75b75f409fb279636746635cbb222
Helen Morris <morris_helen@hotmail.com>
2023-05-02 15:15:53 +0100

    add another test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,17 @@
 from src.utils import party_responses, create_json_file
 from unittest.mock import Mock
 from os import path
+import json
 
-def test_creates_list_of_dicts():
+def test_creates_json_file():
     create_json_file('test-data.json')
     assert path.isfile('test-data.json')
 
+def test_party_responses_adds_a_person_to_json():
+    party_responses('James', 'yes', 'test-data.json')
+    try:
+        json.load('test-data.json')
+        assert True
+    except:
+        print('Not valid JSON object')
+        assert False
