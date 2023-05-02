dda9ff49d705cf431a4dd39ea8e7b77942ded900
Helen Morris <morris_helen@hotmail.com>
2023-05-02 09:46:12 +0100

    create test to check json file created

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,8 @@
-from src.utils import party_responses
+from src.utils import party_responses, create_json_file
 from unittest.mock import Mock
+from os import path
 
 def test_creates_list_of_dicts():
-    
+    create_json_file('test-data.json')
+    assert path.isfile('test-data.json')
+
