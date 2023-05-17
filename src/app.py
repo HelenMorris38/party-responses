eaf0aa4fef72574d85e3eb976a73c804b9baeb61
Helen Morris <morris_helen@hotmail.com>
2023-05-17 12:41:23 +0100

    add blank string validation

--- src/app.py
+++ src/app.py
@@ -7,6 +7,8 @@ while True:
     name = input('What is your name?\nEnter q at anytime to quit.\n')
     if name == 'q':
         break
+    if name == "":
+        name = input('Please enter your name again.\n')
     able_to_attend = input('Can you come to the party?\nPlease enter yes or no.\n')
     if able_to_attend == 'q':
         break
