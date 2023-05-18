947ef81c5573c8051a821f73947b67ab44be7790
Helen Morris <morris_helen@hotmail.com>
2023-05-18 14:04:08 +0100

    remove count var as not needed

--- src/app.py
+++ src/app.py
@@ -1,9 +1,8 @@
 from src.utils import party_responses
 
 filename = 'party-responses.json'
-count = 0
+
 while True:
-    count += 1
     name = input('What is your name?\nEnter q at anytime to quit.\n')
     if name == 'q':
         break
