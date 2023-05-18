7e994e8b6510ff8c62564eae32c3b54446e7bcf7
Helen Morris <morris_helen@hotmail.com>
2023-05-18 14:23:05 +0100

    add able to attend question as a var

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from src.utils import party_responses, update_response
 
 filename = 'party-responses.json'
+able_to_attend_question = 'Can you come to the party?\nPlease enter yes or no.\n'
 welcome_question = input('Hello! Please press "y" if you would like to respond to a party invitation or "u" if you would like to update a previously submitted response.\nEnter q at anytime to quit.\n')
 
 while True:
@@ -10,7 +11,7 @@ while True:
             break
         if name == "":
             name = input('Please enter your name again.\n')
-        able_to_attend = input('Can you come to the party?\nPlease enter yes or no.\n')
+        able_to_attend = input(able_to_attend_question)
         if able_to_attend == 'q':
             break
         else:
@@ -20,7 +21,7 @@ while True:
         id = input('Please enter your id number:\n')
         if id == 'q':
             break
-        updated_response = input('Can you come to the party?\nPlease enter yes or no.\n')
+        updated_response = input(able_to_attend_question)
         if updated_response == 'q':
             break
         else:
