0e8972f37e2f5c1214cb21ada8bc0c9401f47cfd
Helen Morris <morris_helen@hotmail.com>
2023-05-18 14:24:27 +0100

    add line space for readability

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from src.utils import party_responses, update_response
 
 filename = 'party-responses.json'
 able_to_attend_question = 'Can you come to the party?\nPlease enter yes or no.\n'
+
 welcome_question = input('Hello! Please press "y" if you would like to respond to a party invitation or "u" if you would like to update a previously submitted response.\nEnter q at anytime to quit.\n')
 
 while True:
